d063273c27aef5c7b948a63d371360a0d91ff72a
techbaeng <40010543+techbaeng@users.noreply.github.com>
2018-06-08 10:13:29 +0900

    Hello
    Hello Java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
-class  
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java.");
+	}
+
 }
